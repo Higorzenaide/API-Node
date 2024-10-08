fc839260a8b4d8d899c6bed15226a488064596af
Higor Zenaide <higorz.almeida@gmail.com>
2024-10-08 13:59:38 -0300

    arrumado lógica do edit

--- src/models/editClient.js
+++ src/models/editClient.js
@@ -4,27 +4,29 @@ class CreateUser {
         this.data = data; // Armazena os dados recebidos
     }
 
-    async updateUser(userId) { // Adicione um parâmetro para o ID do usuário
-        // Filtra os campos não vazios e transforma em um objeto
+    async updateUser(userId) {
+        // Remove as chaves indesejadas e filtra os campos não nulos ou indefinidos
         const updatedData = Object.fromEntries(
-            Object.entries(this.data) // Converte o objeto em um array de [key, value]
-                .filter(([key, value]) => {
-                    return value
-                }) // Filtra pares chave-valor onde o valor não é vazio
+            Object.entries(this.data)
+                .filter(([key, value]) => 
+                    value !== null && value !== undefined && !['id', 'created_at', 'cpf'].includes(key)
+                )
         );
-        console.log(updatedData)
+    
+        console.log(updatedData); // Verifica os dados filtrados
+    
         try {
             const { error } = await this.client
                 .from('Users')
-                .update(updatedData) // Atualiza apenas os campos que foram passados
-                .eq('id', userId); // Adiciona a condição para atualizar o usuário correto
-
+                .update(updatedData) // Atualiza apenas os campos filtrados
+                .eq('id', userId); // Condição para atualizar o usuário correto
+    
             if (error) {
-                throw error; // Lança o erro para ser capturado pelo catch
+                throw error; // Lança o erro para ser capturado no catch
             }
         } catch (e) {
             console.error('Erro ao atualizar o usuário:', e.message);
-            throw e; // Relança o erro para o tratamento externo
+            throw e; // Relança o erro para tratamento externo
         }
     }
 }
